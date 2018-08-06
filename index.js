ab3b06919223d89bcb520763f0cee751fd6cd939
Pan Cheng <pchengeducation@hotmail.com>
2018-08-06 18:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your solution in this file!
 const driver ={};
+
+function updateDriverWithKeyAndValue(object, key, value){
+  return object.assign({}, driver, {name:"Sam"}, {address:"11 Broadway"})
+}
